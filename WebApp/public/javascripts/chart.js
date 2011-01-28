e1f09c1eace0011a7e3e9ad400661ac3cbd5c35f
prenns <alex.prennsberger@gmail.com>
2011-01-28 17:05:35 +0100

    implementing highcharts

---
+++ WebApp/public/javascripts/chart.js
@@ -0,0 +1,31 @@
+﻿  $(function() {
+    new Highcharts.Chart({
+      chart: {
+        renderTo: "chart"
+      },
+      title: {
+        text: "Messung"
+      },
+      xAxis: {
+        type: "datetime"
+      },
+      yAxis: {
+        title: {
+		  text: "Einheit"
+        }
+      },
+      tooltip: {
+        formatter: function() {
+          return Highcharts.dateFormat("%B %e, %Y", this.x) + ': ' +
+            "" + Highcharts.numberFormat(this.y, 2);
+        }
+      },
+	  series: [
+	  {
+		name: "Messdaten",
+		data: [ 1, 2, 3, 4, 5, 10, 20 ]
+	  }
+
+	  ]
+    });
+  });
